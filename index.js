1def436dbb941dcd13fe7eb9eec6cbba8291ae17
AIT AMEUR Mohamed Ilyas <aitameurmedilyas@gmail.com>
2025-10-30 13:46:36 +0100

    init

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require("express")
+const app = express();
+const PORT = process.env.PORT || 3000;
+app.get("/", (req, res) => {
+    res.send("\n Bonjour depuis Render!");
+});
+app.listen(PORT, () => {
+    console.log('Serveur lance sur le port ${PORT}');
+})
